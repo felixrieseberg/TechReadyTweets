c6ab763e5424995c041b24b80950bb84c4e8f37c
Jason <git@jasonpoon.ca>
2014-12-11 15:26:40 -0800

    start node directly from subfolder

---
+++ server/app.js
@@ -0,0 +1,14 @@
+var fortune = require('fortune'),
+	express = require('express');
+
+var app = fortune ({
+    db: 'techreadytweets'
+})
+.resource('tweet', {
+    author: String,
+    message: String,
+    timestamp: Number 
+});
+
+app.use(express.static(__dirname + '/../client/dist'));
+app.listen(process.env.PORT || 8888);
