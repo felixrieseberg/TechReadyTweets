554297228689c6e52989287040f75b723bbf514a
Felix Rieseberg <felix@felixrieseberg.com>
2014-12-10 16:40:43 -0800

    Rest Adapter

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -1,9 +1,12 @@
 import App from '../app';
 import DS from 'ember-data';
 
+export default DS.RESTAdapter.extend({
+    host: 'http://localhost:1337'
+});
 
 // Uncomment to use local storage!
-App.ApplicationSerializer = DS.LSSerializer.extend();
-export default DS.LSAdapter.extend({
-    namespace: 'TechReadyTwitter'
-});
+// App.ApplicationSerializer = DS.LSSerializer.extend();
+// export default DS.LSAdapter.extend({
+//     namespace: 'TechReadyTwitter'
+// });
