f730b8b9037dea9b419225b44224b0c1117283fb
Felix Rieseberg <felix@felixrieseberg.com>
2014-12-10 16:14:03 -0800

    Minor Fixes

--- client/app/adapters/application.js
+++ client/app/adapters/application.js
@@ -1,7 +1,6 @@
 import App from '../app';
 import DS from 'ember-data';
 
-
 // Uncomment to use local storage!
 App.ApplicationSerializer = DS.LSSerializer.extend();
 export default DS.LSAdapter.extend({
--- client/app/controllers/timeline.js
+++ client/app/controllers/timeline.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
     tweetDisabled: function () {
         var name = this.get('name'),
             draft = this.get('tweetDraft');
