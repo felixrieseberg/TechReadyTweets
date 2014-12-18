6de3bd2603bf29fe98ef2a6c3e8bcdba709f72a3
Jason <github@jasonpoon.ca>
2014-12-17 18:44:08 -0800

    server v2

---
+++ server/v2/TechReadyTweets/models/tweet.js
@@ -0,0 +1,29 @@
+﻿var azure = require('azure-storage');
+var entGen = azure.TableUtilities.entityGenerator;
+
+function Tweet(author, message, timestamp, partitionKey, rowKey) {
+    this.partitionKey = partitionKey || 'tweet';
+    this.rowKey = rowKey || (new Date(3000, 1) - Date.now()).toString();
+
+    this.id = this.rowKey;
+    this.author = author;
+    this.message = message;
+    this.timestamp = timestamp;
+}
+
+Tweet.prototype.generateEntity = function() {
+    return {
+        PartitionKey: entGen.String(this.partitionKey),
+        RowKey: entGen.String(this.rowKey),
+
+        author: entGen.String(this.author),
+        message: entGen.String(this.message),
+        timestamp: entGen.Int64(this.timestamp),
+    }
+}
+
+Tweet.parseEntity = function (entity) {
+    return new Tweet(entity.author._, entity.message._, entity.timestamp._, entity.PartitionKey._, entity.RowKey._);
+}
+
+module.exports = Tweet;
---
+++ server/v2/TechReadyTweets/routes/tweets.js
@@ -0,0 +1,59 @@
+﻿var express = require('express');
+var azure = require('azure-storage');
+var config = require('../config.js');
+var Tweet = require('../models/tweet.js');
+var tableSvc = azure.createTableService(config.storageAccount, config.storageKey);
+var router = express.Router();
+
+var tableName = 'tweets';
+
+router.get('/', function (req, res) {
+    var query = new azure.TableQuery()
+        .where('PartitionKey eq ?', 'tweet');
+
+    tableSvc.queryEntities(tableName, query, null, function(error, result, response) {
+        if (error) {
+            res.status(500).end(error);
+        }
+
+        var tweets = [];
+        for (var i = 0; i < result.entries.length; i++) {
+            var item = result.entries[i];
+            tweets.push(Tweet.parseEntity(item));
+        }
+
+        res.json({ tweets: tweets });
+    });
+});
+
+router.post('/', function (req, res) {
+    if (!req.body.tweet) {
+        res.status(400).end();
+    }
+    
+    var author = req.body.tweet.author;
+    var message = req.body.tweet.message;
+    var timestamp = req.body.tweet.timestamp;
+    
+    if (!author || !message || !timestamp) {
+        res.status(400).end();
+    }
+
+    tableSvc.createTableIfNotExists(tableName, function (error, result, response) {
+        if (error) {
+            res.status(500).end(error);
+        }
+
+        var entity = new Tweet(author, message, timestamp);
+        tableSvc.insertEntity(tableName, entity.generateEntity(), function (error, result, response) {
+            if (error) {
+                res.status(500).end(error);
+            }
+            
+            res.status(201).json({ tweets: entity}).end();
+        });
+    });
+});
+
+
+module.exports = router;
