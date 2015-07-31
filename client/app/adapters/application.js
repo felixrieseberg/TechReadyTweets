257caa7559561243ee828a14c248d8a5087886ff
Jason Poon <jason.poon@microsoft.com>
2015-07-31 09:44:22 -0700

    empty repo

--- client/app/adapters/application.js
+++
@@ -1,6 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.RESTAdapter.extend({})
-
-// Uncomment to run locally!
-// export default DS.FixtureAdapter.extend({});
--- server/models/tweet.js
+++
@@ -1,8 +0,0 @@
-﻿var tweet = function (author, message) {
-    this.author = author;
-    this.message = message;
-    this.timestamp = Date.now().toString();
-    this.id = this.timestamp;
-}
-
-module.exports = tweet;
